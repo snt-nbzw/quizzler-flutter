410a8d99908cbced11975a8534c8aa110f21d433
snt-nbzw <sntnbzw@gmail.com>
2020-11-18 10:19:47 +0900

    105. Encapsulation in Action

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -68,8 +66,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == true) {
                   print('user got it right!');
@@ -77,11 +74,9 @@ class _QuizPageState extends State<QuizPage> {
                   print('user got it wrong');
                 }
 
-                print(questionNumber);
-
                 //The user picked true.
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -100,8 +95,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == true) {
                   print('user got it right!');
@@ -111,7 +105,7 @@ class _QuizPageState extends State<QuizPage> {
 
                 //The user picked false.
                 setState(() {
-                  questionNumber--;
+                  // questionNumber--;
                 });
               },
             ),
